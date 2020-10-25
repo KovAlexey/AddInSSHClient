da2b45c7d9ecf79567db3d90e1fb8ee2cd5cfec0
Alexey <kovalexei@gmail.com>
2020-10-25 12:06:11 +1000

    -

--- src/CAddInSSH.cpp
+++ src/CAddInSSH.cpp
@@ -356,7 +356,7 @@ int32_t CAddInNative::connectToSSH()
 
 	
 	SetLocale(LC_ALL);
-	if (libssh2_userauth_password(session, "Алексей", "pass"))
+	if (libssh2_userauth_password(session, "Алексей", "pass2"))
 		ret = -2;
 
 	delete user;
